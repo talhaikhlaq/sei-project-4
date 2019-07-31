4236862f2e6ed0501fe62bfba493500656cf9767
TalhaIkhlaq <talhaikhlaq@gmail.com>
2019-07-31 14:21:38 +0100

    deployment test

--- config/router.py
+++ config/router.py
@@ -1,6 +1,16 @@
+import os
 from app import app
 from controllers import auth, salaries, categories
 
 app.register_blueprint(auth.api, url_prefix='/api')
 app.register_blueprint(salaries.api, url_prefix='/api')
 app.register_blueprint(categories.api, url_prefix='/api')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+
+    if os.path.isfile('dist/' + path):
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')
